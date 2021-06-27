d6e95e4d21de1454b3d3a101d248727de1c16710
shailunegi <shailunegi@gmail.com>
2021-06-27 21:46:39 +0530

    Update nodejs.groovy

--- job-dsl/nodejs.groovy
+++ job-dsl/nodejs.groovy
@@ -1,6 +1,6 @@
 job('NodeJS example') {
     scm {
-        git('git://github.com/shailunegi/jenkinscourse.git') {  node -> // is hudson.plugins.git.GitSCM
+        git('git://github.com/shailunegi/DockerDemo.git') {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('DSL User')
             node / gitConfigEmail('awssnegi23@gmail.com')
         }
